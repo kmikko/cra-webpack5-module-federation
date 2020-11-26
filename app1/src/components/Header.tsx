813b86c9571e3da1c1ff28790ccb01e0981046a1
Mikko Korhonen <mikko.korhonen@gmail.com>
2020-11-26 13:07:05 +0200

    Copy main to app1

---
+++ app1/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => (
+  <div className="block">
+    <header className="header">Main app</header>
+  </div>
+);
+
+export default Header;
