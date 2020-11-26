81450cf3b1448a0721e245afc180e1fb198b99fb
Mikko Korhonen <mikko.korhonen@gmail.com>
2020-11-26 13:48:24 +0200

    Import app1/Header in main

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 
 import Header from "./components/Header";
+const Header1 = React.lazy(() => import("app1/Header"));
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <React.Suspense fallback="Loading Header1">
+        <Header1 />
+      </React.Suspense>
     </div>
   );
 }
