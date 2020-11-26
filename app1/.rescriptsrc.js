3af9dfc4eb955c4ae8fdcb4f9be8596226034328
Mikko Korhonen <mikko.korhonen@gmail.com>
2020-11-26 13:34:54 +0200

    Expose Header component from app1

--- app1/.rescriptsrc.js
+++ app1/.rescriptsrc.js
@@ -1,15 +1,21 @@
 const { appendWebpackPlugin } = require("@rescripts/utilities");
 const { ModuleFederationPlugin } = require("webpack").container;
+const packageJson = require("./package.json");
 
-module.exports = (config) =>
-  appendWebpackPlugin(
+module.exports = (config) => {
+  config.output.publicPath = `//${process.env.HOST}:${process.env.PORT}/`;
+  return appendWebpackPlugin(
     new ModuleFederationPlugin({
-      name: "main",
-      remotes: {
-        app1: "app1",
-        app2: "app2",
+      name: packageJson.name,
+      library: { type: "var", name: packageJson.name },
+      filename: "remoteEntry.js",
+      exposes: {
+        Header: "./src/components/Header",
       },
       shared: ["react", "react-dom", "react/jsx-dev-runtime"],
     }),
     config
   );
+
+  return config;
+};
